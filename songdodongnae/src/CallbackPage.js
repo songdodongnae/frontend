115c18303af732688dd81ee154fb120d1c7d5751
jiyoung <jiyoung2004120@gmail.com>
2025-01-27 15:31:02 +0900

    feat: 쿠키로 access token 받기 구현

--- songdodongnae/src/CallbackPage.js
+++ songdodongnae/src/CallbackPage.js
@@ -1,12 +1,17 @@
 import React, {useEffect} from "react";
-import { saveToken } from "./TokenStorage";
+import { saveToken } from "./tokenStorage";
 
 const CallbackPage = () => {
     useEffect(() => {
-        saveToken();
+        console.log("saveToken 전");
+        setTimeout(() => {
+            saveToken();  // 쿠키를 읽어서 localStorage에 저장
+            console.log("saveToken 후");
+        }, 1000);
     }, []);
 
     const token = localStorage.getItem('accessToken');
+    console.log("localStorage에서 가져온 토큰:", token);
     return (
         <div>
             <h3>{token}</h3>
@@ -14,4 +19,4 @@ const CallbackPage = () => {
     );
 };
 
-export default CallbackPage
+export default CallbackPage 

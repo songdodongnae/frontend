8b85e0936fc8a62e34a9a2fe2a1cefda5e88fa84
Jiyoung <jiyoung2004120@gmail.com>
2025-02-05 09:41:42 +0900

    Fix: 소셜 로그인 요청

--- songdodongnae/src/component/AdBanner.js
+++ songdodongnae/src/component/AdBanner.js
@@ -3,14 +3,14 @@ import useGet from '../hooks/useGet.js';
 import urls from '../config/apiUrls.json';
 
 const AdBanner = () => {
-    const { posts, loading, error } = useGet(urls['dummy-img-url'], "/800/600");
+    /**const { posts, loading, error } = useGet(urls['dummy-img-url'], "/800/600");
     if (loading) {
         return <div>Loading...</div>; // 로딩 중일 때 표시할 UI
     }
 
     if (error) {
         return <div>Error: {error.message}</div>; // 에러가 있을 때 표시할 UI
-    }
+    }*/
     return (
         <div>
             
--- songdodongnae/src/component/SocialLoginBtn.js
+++ songdodongnae/src/component/SocialLoginBtn.js
@@ -12,9 +12,15 @@ import kakaoLogo from '../images/kakao-logo.svg';
 import useGet from '../hooks/useGet.js';
 
 const SocialLoginBtn = () => {
-  const handleButtonClick = async () => {
+  const handleButtonClick = async (url) => {
     try {
-      const response = await axios.get(s_urls['front-server']+'oauth2', {
+      const response = await axios.get(urls['login-base']+urls[url], {
+        /**
+         * CORS 설정이 필요하면 켜줍니다.
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        }, */
         withCredentials: true,  // 쿠키 포함 (필요 시)
       });
 
@@ -31,9 +37,9 @@ const SocialLoginBtn = () => {
           <div>송도동네에<br/>오신 것을 환영해요!</div>
         </div>
         <div className='login-buttons'>
-          <span className='google-button' onClick={handleButtonClick}><img src={googleLogo} width="20px" height="20px"/>구글 로그인</span>
-          <span className='naver-button' onClick={handleButtonClick}><img src={naverLogo} width="16px" height="16px"/>네이버 로그인</span>
-          <span className='kakao-button' onClick={handleButtonClick}><img src={kakaoLogo} width="18px" height="18px"/>카카오 로그인</span>
+          <span className='google-button' onClick={() => handleButtonClick('google-login')}><img src={googleLogo} width="20px" height="20px"/>구글 로그인</span>
+          <span className='naver-button' onClick={() => handleButtonClick('naver-login')}><img src={naverLogo} width="16px" height="16px"/>네이버 로그인</span>
+          <span className='kakao-button' onClick={() => handleButtonClick('kakao-login')}><img src={kakaoLogo} width="18px" height="18px"/>카카오 로그인</span>
         </div>
       </div>
       <div className='without-login'>

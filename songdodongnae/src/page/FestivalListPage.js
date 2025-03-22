3d35553ae7cce1d3e978dc51956b29ef97e9d499
jiyoung <jiyoung2004120@gmail.com>
2025-03-22 16:04:22 +0900

    Style: 북마크 추가

--- songdodongnae/src/page/FestivalListPage.js
+++ songdodongnae/src/page/FestivalListPage.js
@@ -4,6 +4,7 @@ import Navigation from "../component/Navigation.js";
 import Footer from "../component/Footer.js";
 import FestivalCalendar from "../component/FestivalCalendar.js";
 import "../css/FestivalListPage.css"
+import bookmark from "../images/festivalBookmark.svg";
 
 const FestivalListPage = () => {
 
@@ -26,6 +27,10 @@ const FestivalListPage = () => {
                     <div key={fes.id} className="festivals-item">
                         <div className="festivals-img-container">
                             <img className="festivals-thumbnail" src={fes.download_url} alt={fes.title} />
+                            <div className="festival-bookmark">
+                                <div className="festival-bookmark-dot" />
+                                <img className="festival-bookmark-icon" src={bookmark} />
+                            </div>
                         </div>
                         <div className="festivals-info-container">
                             <div className="festivals-item-title">{fes.author}</div>

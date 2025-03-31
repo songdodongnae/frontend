20d790d6ba3c5916ff4538ea0f12fb6c4c689126
jiyoung <jiyoung2004120@gmail.com>
2025-03-31 19:50:15 +0900

    Feat: 세부 페이지

--- songdodongnae/src/page/CreatorPage.js
+++ songdodongnae/src/page/CreatorPage.js
@@ -3,13 +3,23 @@ import Header from "../component/Header";
 import Navigation from "../component/Navigation";
 import Footer from "../component/Footer";
 
+import { useNavigate } from "react-router-dom";
+
 const CreatorPage = () => {
 
     return (
         <div>
             <Header />
-            <Navigation />
-            크리에이터 상세 페이지
+            <Navigation /> 
+            <div className="creator-title-text">
+                <div className="creator-title-text-1">크리에이터</div>
+                <div className="creator-title-text-2">관심있는 크리에이터의 글을 읽어보세요.</div>
+            </div>
+            <div className="creator-mascotte">
+                <div className="creator-mascotte-title">
+                    <div className="creator-mascotte-title-text">송도동네 친구들</div>
+                </div>
+            </div>
             <Footer />
         </div>
     )
